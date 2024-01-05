7da6db5e16d8dc362533dee31347cc03b849ca5f
JakeW <JakeW@DESKTOP-UMJBC9A>
2024-01-05 18:29:06 -0500

    Add project files.

---
+++ TextFileToExcel/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileToExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextFile textFile = new TextFile(args[0]);
+
+        }
+    }
+}
---
+++ TextFileToExcel/TextFile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileToExcel
+{
+    public class TextFile
+    {
+        private string fileName;
+        private string filePath;
+
+        public TextFile(string path)
+        {
+            if(File.Exists(path))
+            {
+                filePath = path;
+                fileName = Path.GetFileName(path);
+            }
+            else
+            {
+                Console.WriteLine("File does not exist! Please reference an existing file!");
+            }
+        }
+
+        public void OpenFileForReading()
+        {
+            try
+            {
+                File.OpenRead(filePath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("There was a problem opening the file for reading!");
+                throw e;
+            }
+        }
+
+        public int GetValuesPerRow()
+        {
+            using (var streamReader = File.OpenText(filePath))
+            {
+                string[] lines = streamReader.ReadToEnd().Split("\r\n".ToCharArray());
+            }
+
+        }
+
+    }
+}
